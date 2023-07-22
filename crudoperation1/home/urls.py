e2fb9c51972434f29766b4c035571895bfd040a6
shivani31 <shivisahu31.ss@gmail.com>
2023-07-21 21:05:05 -0700

    User

--- crudoperation1/home/urls.py
+++ crudoperation1/home/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('registration/',views.registration),
     path('login/',views.login),
     path('table/',views.table),
+    path('update/<int:uid>/',views.update_views),
+    path('update/',views.Update_form_data),
+    path('delete/<int:pk>/',views.delete_user,name="delete"),
 ]
--- crudoperation1/home/views.py
+++ crudoperation1/home/views.py
@@ -26,3 +26,18 @@ def table(request):
     data = User.objects.all()
     return render(request,'table.html',{"data":data})
 
+def update_views(request,uid):
+    res = User.objects.get(id=uid)
+    return render(request,'update.html',{"data":res})
+
+def Update_form_data(request):
+    if request.method == "POST":
+        uid=request.POST["uid"]
+        name=request.POST["name"]
+        email=request.POST["email"]
+        User.objects.filter(id=uid).update(name=name,email=email)
+        return redirect("/table/")
+    
+def delete_user(request,pk):
+    User.objects.filter(id=pk).delete()
+    return redirect("/table/")
